1c6ebaef3c1d26c498115a354fa704c8ecd08799
Tarun Gupta <tarun.gupta093@gmail.com>
2023-09-21 16:34:32 +0530

    removed unused import

--- apps/vid-search/src/vid-search.controller.ts
+++ apps/vid-search/src/vid-search.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { VidSearchService } from './vid-search.service';
 import { Video } from './dtos/video-search.dto';
 import { MessagePattern } from '@nestjs/microservices';
